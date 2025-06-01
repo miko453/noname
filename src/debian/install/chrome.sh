49d135e4dbadd807157bf4512429d1f2f1e5a8b4
Miko Lan <190144771+miko453@users.noreply.github.com>
2025-06-01 04:35:39 +0000

    Update chrome.sh

--- src/debian/install/chrome.sh
+++ src/debian/install/chrome.sh
@@ -3,7 +3,4 @@
 set -e
 
 echo "Install Google Chrome"
-wget -O /tmp/1.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y /tmp/1.deb
-apt-get clean -y
-rm -rf /tmp/1.deb
+apt install -y chromium
